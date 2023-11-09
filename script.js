22708d42bba8643e211e2775b3a6cd0c43243051
varshakanugula <150398821+varshakanugula@users.noreply.github.com>
2023-11-10 10:53:39 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeText() {
+    // this should change the displlay text
+    let textList = ['text1','text2','text3','text4','text5'];
+    let randomNumber = getRandomNumber(0, textList.length - 1);
+    console.log(randomNumber);
+    document.getElementById('header').innerHTML = textList[randomNumber];
+}
+
+function getRandomNumber(min,max) {
+    // generate a random number between a range
+    return Math.floor(Math.random()*(max-min + 1)+min);
+}
